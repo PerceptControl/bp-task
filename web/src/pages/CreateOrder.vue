6de0975a5e1a8b0dff0bad6f79b13348cc507749
PerceptionControl <your_email@example.com>
2023-10-25 12:51:22 +0300

    fix url

--- web/src/pages/CreateOrder.vue
+++ web/src/pages/CreateOrder.vue
@@ -55,7 +55,7 @@ export default defineComponent({
 
     function onSubmit() {
       loading.value = true;
-      fetch("https://unknown-dorothea-eg.koyeb.app//orders/create", {
+      fetch("https://unknown-dorothea-eg.koyeb.app/orders/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
